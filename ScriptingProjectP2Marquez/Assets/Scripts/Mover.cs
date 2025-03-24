a901c85afba9090213948bd54a592af69d4f6dcd
Richie <722461@student.dusd.net>
2025-03-24 09:59:14 -0700

    Finished Delta move

--- ScriptingProjectP2Marquez/Assets/Scripts/Mover.cs
+++ ScriptingProjectP2Marquez/Assets/Scripts/Mover.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class YetAnotherScript : MonoBehaviour
+public class Mover : MonoBehaviour
 {
+    public float speed = 5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class YetAnotherScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += Vector3.forward * (speed * Time.deltaTime);
     }
 }
